c35242af22e90fb0245e808c032813912592e038
TDeS-app <thedailyecomstore@gmail.com>
2025-07-08 15:59:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -86,6 +86,6 @@ if not product_df.empty and st.session_state.selected_handles:
         else:
             raw_inventory = pd.DataFrame()
         matched_inventory = raw_inventory[
-            raw_inventory["Variant SKU"].apply(lambda s: any(str(s).find(sku) == 0 for sku in selected_skus))
+            raw_inventory["SKU"].apply(lambda s: any(str(s).find(sku) == 0 for sku in selected_skus))
         ]
         st.download_button("📦 Download Matching Inventory CSV", data=matched_inventory.to_csv(index=False).encode("utf-8"), file_name="matching_inventory.csv")
